941a613b184d2973b0f73ccd0e19d47635b86294
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2013-12-06 15:09:22 -0500

    Cleaned up some types

--- src/main/java/us/chotchki/springWeb/init/FixedAnnotationConfig.java
+++ src/main/java/us/chotchki/springWeb/init/FixedAnnotationConfig.java
@@ -21,77 +21,71 @@ public class FixedAnnotationConfig extends AnnotationConfiguration {
 	private static final Logger LOG = Log.getLogger(AnnotationConfiguration.class);
 	
 	@Override
-	public void createServletContainerInitializerAnnotationHandlers(
-			WebAppContext context, List<ServletContainerInitializer> scis)
-			throws Exception {
-		if (scis == null || scis.isEmpty())
-			return; // nothing to do
+    public void createServletContainerInitializerAnnotationHandlers (WebAppContext context, List<ServletContainerInitializer> scis)
+    throws Exception
+    {
+        if (scis == null || scis.isEmpty())
+            return; // nothing to do
 
-		List<ContainerInitializer> initializers = new ArrayList<ContainerInitializer>();
-		context.setAttribute(CONTAINER_INITIALIZERS, initializers);
+        List<ContainerInitializer> initializers = new ArrayList<ContainerInitializer>();
+        context.setAttribute(CONTAINER_INITIALIZERS, initializers);
 
-		for (ServletContainerInitializer service : scis) {
-			HandlesTypes annotation = service.getClass().getAnnotation(
-					HandlesTypes.class);
-			ContainerInitializer initializer = null;
-			if (annotation != null) {
-				// There is a HandlesTypes annotation on the on the
-				// ServletContainerInitializer
-				Class[] classes = annotation.value();
-				if (classes != null) {
-					initializer = new ContainerInitializer(service, classes);
-					initializer.addApplicableTypeName(WebAppInit.class.getCanonicalName());
+        for (ServletContainerInitializer service : scis)
+        {
+            HandlesTypes annotation = service.getClass().getAnnotation(HandlesTypes.class);
+            ContainerInitializer initializer = null;
+            if (annotation != null)
+            {    
+                //There is a HandlesTypes annotation on the on the ServletContainerInitializer
+                Class<?>[] classes = annotation.value();
+                if (classes != null)
+                {
+                    initializer = new ContainerInitializer(service, classes);
+                    initializer.addApplicableTypeName(WebAppInit.class.getCanonicalName());
 
-					// If we haven't already done so, we need to register a
-					// handler that will
-					// process the whole class hierarchy to satisfy the
-					// ServletContainerInitializer
-					if (context.getAttribute(CLASS_INHERITANCE_MAP) == null) {
-						// MultiMap<String> map = new MultiMap<>();
-						ConcurrentHashMap<String, ConcurrentHashSet<String>> map = new ConcurrentHashMap<String, ConcurrentHashSet<String>>();
-						context.setAttribute(CLASS_INHERITANCE_MAP, map);
-						_classInheritanceHandler = new ClassInheritanceHandler(
-								map);
-					}
+                    //If we haven't already done so, we need to register a handler that will
+                    //process the whole class hierarchy to satisfy the ServletContainerInitializer
+                    if (context.getAttribute(CLASS_INHERITANCE_MAP) == null)
+                    {
+                        //MultiMap<String> map = new MultiMap<>();
+                        ConcurrentHashMap<String, ConcurrentHashSet<String>> map = new ConcurrentHashMap<String, ConcurrentHashSet<String>>();
+                        context.setAttribute(CLASS_INHERITANCE_MAP, map);
+                        _classInheritanceHandler = new ClassInheritanceHandler(map);
+                    }
 
-					for (Class c : classes) {
-						// The value of one of the HandlesTypes classes is
-						// actually an Annotation itself so
-						// register a handler for it
-						if (c.isAnnotation()) {
-							if (LOG.isDebugEnabled())
-								LOG.debug("Registering annotation handler for "
-										+ c.getName());
-							_containerInitializerAnnotationHandlers
-									.add(new ContainerInitializerAnnotationHandler(
-											initializer, c));
-						}
-					}
-				} else {
-					initializer = new ContainerInitializer(service, null);
-					if (LOG.isDebugEnabled())
-						LOG.debug("No classes in HandlesTypes on initializer "
-								+ service.getClass());
-				}
-			} else {
-				initializer = new ContainerInitializer(service, null);
-				if (LOG.isDebugEnabled())
-					LOG.debug("No annotation on initializer "
-							+ service.getClass());
-			}
-
-			initializers.add(initializer);
-		}
-
-		// add a bean to the context which will call the
-		// servletcontainerinitializers when appropriate
-		ServletContainerInitializersStarter starter = (ServletContainerInitializersStarter) context
-				.getAttribute(CONTAINER_INITIALIZER_STARTER);
-		if (starter != null)
-			throw new IllegalStateException(
-					"ServletContainerInitializersStarter already exists");
-		starter = new ServletContainerInitializersStarter(context);
-		context.setAttribute(CONTAINER_INITIALIZER_STARTER, starter);
-		context.addBean(starter, true);
-	}
+                    for (Class<?> c: classes)
+                    {
+                        //The value of one of the HandlesTypes classes is actually an Annotation itself so
+                        //register a handler for it
+                        if (c.isAnnotation())
+                        {
+                            if (LOG.isDebugEnabled()) LOG.debug("Registering annotation handler for "+c.getName());
+                           _containerInitializerAnnotationHandlers.add(new ContainerInitializerAnnotationHandler(initializer, c));
+                        }
+                    }
+                }
+                else
+                {
+                    initializer = new ContainerInitializer(service, null);
+                    if (LOG.isDebugEnabled()) LOG.debug("No classes in HandlesTypes on initializer "+service.getClass());
+                }
+            }
+            else
+            {
+                initializer = new ContainerInitializer(service, null);
+                if (LOG.isDebugEnabled()) LOG.debug("No annotation on initializer "+service.getClass());
+            }
+            
+            initializers.add(initializer);
+        }
+        
+        
+        //add a bean to the context which will call the servletcontainerinitializers when appropriate
+        ServletContainerInitializersStarter starter = (ServletContainerInitializersStarter)context.getAttribute(CONTAINER_INITIALIZER_STARTER);
+        if (starter != null)
+            throw new IllegalStateException("ServletContainerInitializersStarter already exists");
+        starter = new ServletContainerInitializersStarter(context);
+        context.setAttribute(CONTAINER_INITIALIZER_STARTER, starter);
+        context.addBean(starter, true);
+    }
 }
