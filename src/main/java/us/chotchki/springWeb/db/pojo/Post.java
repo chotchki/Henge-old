8a7e9b666770c1589dd7a4847f0664a862ecd2c9
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2013-12-15 18:41:42 -0500

    Finished the post pojo

--- src/main/java/us/chotchki/springWeb/db/pojo/Post.java
+++ src/main/java/us/chotchki/springWeb/db/pojo/Post.java
@@ -1,6 +1,35 @@
 package us.chotchki.springWeb.db.pojo;
 
+import org.joda.time.Instant;
+
 public class Post {
 	private int id = 0;
+	private Instant published = null;
+	private String title = null;
+	private String content = null;
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Instant getPublished() {
+		return published;
+	}
+	public void setPublished(Instant published) {
+		this.published = published;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
