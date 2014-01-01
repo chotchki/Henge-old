b081b0ceaf8178e6c9b4f6f2294b79e6355d27f6
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2014-01-01 16:41:41 -0500

    Made the thymeleaf cache turn on automatically

--- src/main/java/us/chotchki/springWeb/init/spring/ThymeleafConfig.java
+++ src/main/java/us/chotchki/springWeb/init/spring/ThymeleafConfig.java
@@ -2,6 +2,8 @@ package us.chotchki.springWeb.init.spring;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.extras.springsecurity3.dialect.SpringSecurityDialect;
@@ -11,6 +13,7 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 public class ThymeleafConfig {
+	private static final Logger log = LoggerFactory.getLogger(ThymeleafConfig.class);
 
 	@Bean
 	public ServletContextTemplateResolver templateResolver() {
@@ -19,7 +22,17 @@ public class ThymeleafConfig {
 		resolver.setSuffix(".html");
 		resolver.setTemplateMode("HTML5");
 		resolver.setOrder(1);
-		resolver.setCacheable(false);
+		
+		String os = System.getProperty("os.name");
+		if(os.startsWith("Windows")){
+			log.debug("Disabling Thymeleaf Cache");
+			resolver.setCacheable(false);
+		} else {
+			log.debug("Enabling Thymeleaf Cache");
+			resolver.setCacheable(true);
+		}
+		
+		
 		return resolver;
 	}
 
